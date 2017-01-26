01b4aed03cc0d2c83f7e9e4636768f9c8863ffb3
musyoku <musyoku@users.noreply.github.com>
2017-01-27 02:04:41 +0900

    :v:

--- train_animeface/args.py
+++ train_animeface/args.py
@@ -3,10 +3,10 @@ import argparse
 
 # Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--gpu-device", type=int, default=0)
-parser.add_argument("--image-dir", type=str, default="images")
-parser.add_argument("--model-dir", type=str, default="model")
-parser.add_argument("--plot-dir", type=str, default="plot")
+parser.add_argument("-g", "--gpu-device", type=int, default=0)
+parser.add_argument("-m", "--model-dir", type=str, default="model")
+parser.add_argument("-p", "--plot-dir", type=str, default="plot")
+parser.add_argument("-i", "--image-dir", type=str, default="images")
 parser.add_argument("-k", "--unrolling-steps", type=int, default=10)
 
 # seed
